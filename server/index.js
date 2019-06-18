d78132e68bb0094a6613b7940b2d23b8fc27efe0
root <root@ip-172-31-8-163.us-west-1.compute.internal>
2019-06-18 22:41:36 +0000

    refactor for AWS deployment

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require('path');
 const proxy = require('http-proxy-middleware');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
-app.use('/*/reservations*', proxy('http://localhost:3020'));
+app.use('/*/reservations*', proxy('ec2-54-153-75-57.us-west-1.compute.amazonaws.com'));
 
 app.get('/:id', (req, res) => {
   if (!req.params.id) {
